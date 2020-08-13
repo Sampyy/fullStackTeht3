a87cc420ffecdd8888d0cb0dd2388906529bfb6c
Sami <samialeksi.tuomela@gmail.com>
2020-08-13 19:56:54 +0300

    fixed get

---
+++ build/precache-manifest.234511543c80116b21b5dde578458a2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88aa580ee54caf6457a0c0e7ad21154",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de1d82bd36534a3c066f",
+    "url": "/static/css/main.134e9fdd.chunk.css"
+  },
+  {
+    "revision": "f5f7b13d34d14b56302e",
+    "url": "/static/js/2.076799b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.076799b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1d82bd36534a3c066f",
+    "url": "/static/js/main.8b0f8671.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
